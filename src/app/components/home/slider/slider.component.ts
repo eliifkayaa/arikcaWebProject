fbd62c496017c90448a8ce90c14c0a50df96fa31
Elif Gokce <e_kaya0@outlook.com>
2024-08-01 10:06:41 +0300

    swiper bugs fixed

--- src/app/components/home/slider/slider.component.ts
+++ src/app/components/home/slider/slider.component.ts
@@ -1,81 +1,86 @@
-import { Component, AfterViewInit  } from '@angular/core';
+import { Component, AfterViewInit , Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { SwiperOptions } from 'swiper/types';
+import Swiper from 'swiper';
 
-// import Swiper, { Navigation, Pagination, Autoplay, Parallax } from 'swiper';
-// import type { SwiperOptions } from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 
-// import * as $ from 'jquery';
-
-
-// Swiper.use([Navigation, Pagination, Autoplay, Parallax]);
-
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import  $ from 'jquery';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss'
 })
-export class SliderComponent  {
+export class SliderComponent implements AfterViewInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
-    // ngAfterViewInit() {
-    //   // HERO SLIDER
-    //   const menu: string[] = [];
-    //   $('.swiper-slide').each(function(index: number) {
-    //     menu.push($(this).find('.slide-inner').attr("data-text") as string);
-    //   });
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // HERO SLIDER
+      const menu: string[] = [];
+      $('.swiper-slide').each((index: number, element: HTMLElement) => {
+        menu.push($(element).find('.slide-inner').attr('data-text') as string);
+      });
 
-  
-    //   const interleaveOffset = 0.5;
-    //   const swiperOptions: SwiperOptions = {
-    //     loop: true,
-    //     speed: 1000,
-    //     parallax: true,
-    //     autoplay: {
-    //       delay: 6500,
-    //       disableOnInteraction: false,
-    //     },
-    //     watchSlidesProgress: true,
-    //     pagination: {
-    //       el: '.swiper-pagination',
-    //       clickable: true,
-    //     },
-    //     navigation: {
-    //       nextEl: '.swiper-button-next',
-    //       prevEl: '.swiper-button-prev',
-    //     },
-    //     on: {
-    //       progress(swiper) {
-    //         for (let i = 0; i < swiper.slides.length; i++) {
-    //           const slideProgress = swiper.slides[i].progress;
-    //           const innerOffset = swiper.width * interleaveOffset;
-    //           const innerTranslate = slideProgress * innerOffset;
-    //           swiper.slides[i].querySelector<HTMLElement>(".slide-inner").style.transform =
-    //             `translate3d(${innerTranslate}px, 0, 0)`;
-    //         }
-    //       },
-    //       touchStart(swiper) {
-    //         for (let i = 0; i < swiper.slides.length; i++) {
-    //           swiper.slides[i].style.transition = "";
-    //         }
-    //       },
-    //       setTransition(swiper, speed) {
-    //         for (let i = 0; i < swiper.slides.length; i++) {
-    //           swiper.slides[i].style.transition = `${speed}ms`;
-    //           swiper.slides[i].querySelector<HTMLElement>(".slide-inner").style.transition =
-    //             `${speed}ms`;
-    //         }
-    //       }
-    //     }
-    //   };
-  
-    //   const swiper = new Swiper('.swiper-container', swiperOptions);
-  
-    //   // DATA BACKGROUND IMAGE
-    //   const sliderBgSetting = $(".slide-bg-image");
-    //   sliderBgSetting.each(function(indx) {
-    //     if ($(this).attr("data-background")) {
-    //       $(this).css("background-image", `url(${ $(this).data("background") })`);
-    //     }
-    //   });
-    // }
+      const interleaveOffset = 0.5;
+      const swiperOptions: SwiperOptions = {
+        loop: true,
+        speed: 1000,
+        parallax: true,
+        autoplay: {
+          delay: 6500,
+          disableOnInteraction: false,
+        },
+        watchSlidesProgress: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        on: {
+          progress: (swiper: any) => {
+            for (let i = 0; i < swiper.slides.length; i++) {
+              const slideProgress = swiper.slides[i].progress;
+              const innerOffset = swiper.width * interleaveOffset;
+              const innerTranslate = slideProgress * innerOffset;
+              swiper.slides[i].querySelector('.slide-inner').style.transform =
+                `translate3d(${innerTranslate}px, 0, 0)`;
+            }
+          },
+          touchStart: (swiper: any) => {
+            for (let i = 0; i < swiper.slides.length; i++) {
+              swiper.slides[i].style.transition = '';
+            }
+          },
+          setTransition: (swiper: any, speed: number) => {
+            for (let i = 0; i < swiper.slides.length; i++) {
+              swiper.slides[i].style.transition = `${speed}ms`;
+              swiper.slides[i].querySelector('.slide-inner').style.transition =
+                `${speed}ms`;
+            }
+          }
+        }
+      };
+
+      const swiper = new Swiper('.swiper-container', swiperOptions);
+
+      // DATA BACKGROUND IMAGE
+      const sliderBgSetting = $('.slide-bg-image');
+      sliderBgSetting.each((indx: number, element: HTMLElement) => {
+        if ($(element).attr('data-background')) {
+          $(element).css('background-image', `url(${ $(element).data('background') })`);
+        }
+      });
+    }
+  }
 }
+
 
