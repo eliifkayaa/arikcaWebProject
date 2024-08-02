7728a842b539c25b8555d76365521be7469ef1a9
Elif Gokce <e_kaya0@outlook.com>
2024-08-02 17:39:49 +0300

    home-service commit one

--- src/app/components/home/home-components/home-services/home-services.component.ts
+++ src/app/components/home/home-components/home-services/home-services.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-home-services',
   templateUrl: './home-services.component.html',
-  styleUrl: './home-services.component.scss'
+  styleUrls: ['./home-services.component.scss']
 })
-export class HomeServicesComponent {
+export class HomeServicesComponent implements AfterViewInit {
+  
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  ngAfterViewInit(): void {
+   
+  }
+
 
 }
