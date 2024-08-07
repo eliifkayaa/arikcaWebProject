484108fd6d5643006ba5d5c7802e02be3820ea6b
Elif Gokce <e_kaya0@outlook.com>
2024-08-07 17:34:41 +0300

    navbar active hatası düzeltildi

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,15 @@
-import { Component, EventEmitter, Input, Output, QueryList } from '@angular/core';
-import { IsActiveMatchOptions, NavigationEnd, Router, RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
 
   navLinks = [
-    { url: '', label: 'Anasayfa'},
+    { url: '/', label: 'Anasayfa'},
     { url: '/about', label: 'Hakkımızda'},
     { url: '/project', label: 'Projeler'},
     { url: '/services', label: 'Hizmetler'},
@@ -17,12 +17,25 @@ export class NavbarComponent {
     { url: '/contact', label: 'İletişim'},
   ];
 
-  //activeLinks['Anasayfa'] ifadesi, "Anasayfa" linkinin aktif olup olmadığını döndürür.
-  activeLinks: { [key: string]: boolean} = {};
+  activeLinks: { [key: string]: boolean } = {};
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.updateActiveLinks();
+      }
+    });
+  }
 
   onRouterLinkActive(isActive: boolean, link: string) {
     this.activeLinks[link] = isActive;
-    //Burada linklerin yalnızca bir kere yüklendiğinde çalışır, yani navigasyon yapıldığında tekrar çalışmaz.
   }
-  
+
+  private updateActiveLinks() {
+    this.navLinks.forEach(link => {
+      const urlTree = this.router.parseUrl(this.router.url);
+      const isActive = this.router.isActive(link.url, { paths: 'exact', queryParams: 'exact', fragment: 'ignored', matrixParams: 'ignored' });
+      this.activeLinks[link.label] = isActive;
+    });
+  }
 }
